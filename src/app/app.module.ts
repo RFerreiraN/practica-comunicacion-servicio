82d771960d4a6b27176a9b6e9b8b19b5e096c0b6
Ricardo Ferreira <ricardoferreiran@gmail.com>
2025-10-16 22:52:28 +0200

    crearremos pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { PadreComponent } from './padre/padre.component';
 import { HijoComponent } from './hijo/hijo.component';
 import { DirectivasComponent } from './directivas/directivas.component';
+import { EstiloHermanosDirective } from './estilo-hermanos.directive';
+import { PipesComponent } from './pipes/pipes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PadreComponent,
     HijoComponent,
-    DirectivasComponent
+    DirectivasComponent,
+    EstiloHermanosDirective,
+    PipesComponent
   ],
   imports: [
     BrowserModule,
