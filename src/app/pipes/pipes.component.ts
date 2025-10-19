1f335fbabe004cc526a565b7a2fa7b1b029b4fba
Ricardo Ferreira <ricardoferreiran@gmail.com>
2025-10-19 22:45:52 +0200

    terminamos pipes

--- src/app/pipes/pipes.component.ts
+++ src/app/pipes/pipes.component.ts
@@ -6,6 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./pipes.component.css']
 })
 export class PipesComponent {
+  // pipe Personalizado
+  mensaje : string = 'este texto tiene aplicado un pipe personalizado que capitaliza todas las letras';
+  // pipe Date
   fecha : Date = new Date()
+  // pipe Currency
   precio : number = 1000;
+  // pipe Decimal
+  pi : number = Math.PI;
+  // pipe Porcentaje
+  factor : number = 0.7
 }
